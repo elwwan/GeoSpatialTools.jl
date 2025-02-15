5817cd7752dd3c8516318fa1aaee2661d8259fd8
Elwan <87530085+elwwan@users.noreply.github.com>
2025-02-15 20:43:55 +0100

    Added tests for area and length calculation

--- test/runtests.jl
+++ test/runtests.jl
@@ -72,3 +72,38 @@ using Test
         MultiPolygon([Polygon([[(0,0), (1,0), (1,1), (0,1)]])])
     ]), GeometryCollection)
 end
+
+@testset "Area" begin
+    @test_throws MethodError area(Point((0,0)))
+    @test_throws MethodError area(MultiPoint([Point((0,0)), Point((0,0))]))
+    @test_throws MethodError area(LineString([(0,0), (1,0)]))
+    @test_throws MethodError area(MultiLineString([LineString([(0,0), (1,0)])]))
+
+    #Polygon
+    polygon1 = Polygon([[(-1,-1), (2,-1), (2,2), (-1,2)], [(0,0), (1,0), (1,1), (0,1)]])
+    @test area(polygon1) == 8.0
+    polygon2 = Polygon([[(6.31,5.41), (9.00,4.50), (11.00,0.50), (7.00,-2.50), (-3.00,-2.00), (-2.50,3.50), (3.00,6.50)]])
+    @test area(polygon2) == 92.41499999999999
+    @test area(polygon2, 2) == 92.42
+    @test area(polygon2, 3) == 92.415
+
+    #MultiPolygon
+    @test area(MultiPolygon([polygon1, polygon2])) == 100.41499999999999
+    @test area(MultiPolygon([polygon1, polygon2]), 2) == 100.42
+    @test area(MultiPolygon([polygon1, polygon2]), 3) == 100.415
+
+end
+
+@testset "Length" begin
+    @test_throws MethodError line_length(Point((0,0)))
+    @test_throws MethodError line_length(MultiPoint([Point((0,0)), Point((0,0))]))
+    @test_throws MethodError line_length(Polygon([[(0,0), (1,0), (1,1), (0,1)]]))
+    @test_throws MethodError line_length(MultiPolygon([Polygon([[(0,0), (1,0), (1,1), (0,1)]]), Polygon([[(0,0), (1,0), (1,1), (0,1)]])]))
+
+    @test line_length(LineString([(0,0), (0,1)])) == 1.0
+    @test line_length(LineString([(0,0), (1,1)]), 5) == 1.41421
+
+    @test line_length(MultiLineString([ LineString([(0,0), (0,1)]), LineString([(0,0), (0,1)]) ])) == 2.0
+    @test line_length(MultiLineString([ LineString([(0,0), (0,1)]), LineString([(0,0), (1,1)]) ]), 5) == 2.41421
+end
+
